0888518a3e6133dee7c10d4b4e1b6a6339bc6d0c
Camila Scaglioni <camila.pia.scaglioni@gmail.com>
2024-05-12 14:34:12 -0300

    titulo y cambios en input del menu de opciones

--- src/ui/ActionInput.gd
+++ src/ui/ActionInput.gd
@@ -4,17 +4,16 @@ extends Node
 onready var input = $HBoxContainer/Panel/Input
 onready var action = $HBoxContainer/Action
 
-export (String) var action_input: String setget _set_action_input
+export (StreamTexture) var action_input: StreamTexture setget _set_action_input
 export (String) var action_name: String setget _set_action_name
 
 func _ready() -> void:
-	input.text = action_input
+	input.texture = action_input
 	action.text = action_name
 
-func _set_action_input (inp: String) -> void:
+func _set_action_input (inp: StreamTexture) -> void:
 	action_input = inp
-	if Engine.editor_hint && has_node("%Input"):
-		$"%Input".text = inp
+	
 		
 func _set_action_name (nm: String) -> void:
 	action_name = nm
